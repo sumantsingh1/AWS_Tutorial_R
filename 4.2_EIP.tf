c4faf9d4b7a2672a92e9a7300a6fc9758c8981bc
Sumant Kumar Singh <30475847+sumantsingh1@users.noreply.github.com>
2024-10-31 11:35:08 +0530

    Create 4.2_EIP.tf

---
+++ 4.2_EIP.tf
@@ -0,0 +1,50 @@
+provider "aws"{
+ region="us-east-1"
+
+}
+
+resource "aws_instance" "mycloudserver" {
+  ami           = "ami-0ddc798b3f1a5117e"
+  instance_type = "t2.micro"
+ // vpc_security_group_ids= [aws_security_group.mycloudserverSG.id]//
+
+  tags = {
+    Name = "mycloudserver"
+  }
+}
+resource "aws_eip" "mycloudservereip" {
+  instance = aws_instance.mycloudserver.id
+  vpc      = true
+}
+/*
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+
+resource "aws_security_group" "mycloudserverSG" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks= ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}*/
